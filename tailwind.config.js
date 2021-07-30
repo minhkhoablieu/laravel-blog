c28c35a7acb70a57437304b50f037583959abc88
Khoa Phung <chanzi909@gmail.com>
2021-07-31 00:54:31 +0700

    change font roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Quicksand", "sans-serif"],
+                sans: ["Roboto", "sans-serif"],
             },
         },
     },
