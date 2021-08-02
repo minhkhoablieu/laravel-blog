97247103cba5b583d988f2739959aaac03d7245c
Khoa Phung <chanzi909@gmail.com>
2021-08-02 17:41:58 +0700

    fix delete slug before delete post, page

--- app/Filament/Resources/PageResource/Pages/EditPage.php
+++ app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\PageResource\Pages;
 
 use App\Filament\Resources\PageResource;
+use App\Models\Slug;
 use Filament\Resources\Pages\EditRecord;
 
 class EditPage extends EditRecord
 {
     public static $resource = PageResource::class;
+
+    protected function beforeDelete()
+    {
+        $page = $this->record;
+
+        $slug = Slug::whereReferenceId($page->id)
+            ->whereReferenceType(self::$resource::getModel())->first();
+        $slug->delete();
+    }
 }
--- app/Filament/Resources/PostResource/Pages/EditPost.php
+++ app/Filament/Resources/PostResource/Pages/EditPost.php
@@ -4,8 +4,17 @@ namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
 use Filament\Resources\Pages\EditRecord;
-
+use App\Models\Slug;
 class EditPost extends EditRecord
 {
     public static $resource = PostResource::class;
+
+    protected function beforeDelete()
+    {
+        $post = $this->record;
+
+        $slug = Slug::whereReferenceId($post->id)
+                        ->whereReferenceType(self::$resource::getModel())->first();
+        $slug->delete();
+    }
 }
